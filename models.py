bed76fa3c0ed113b8d1d2a3fbf9896ed4e7cba14
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-09-26 12:58:00 +0200

    Compositional vision transformers training

--- models.py
+++ models.py
@@ -3,7 +3,6 @@
 import torch
 import torch.nn as nn
 from functools import partial
-import random
 
 from timm.models.vision_transformer import VisionTransformer, _cfg
 from timm.models.registry import register_model
@@ -13,14 +12,11 @@ from timm.models.layers import trunc_normal_
 __all__ = [
     'deit_tiny_patch16_224', 'deit_small_patch16_224', 'deit_base_patch16_224',
     'deit_tiny_distilled_patch16_224', 'deit_small_distilled_patch16_224',
-    'deit_base_distilled_patch16_224', 'deit_base_patch16_384',
-    'deit_base_distilled_patch16_384',
+    'deit_base_distilled_patch16_224', 'deit_base_patch16_384'
 ]
 
-from vision_transformer import CompVisionTransformer
 
-
-class DistilledVisionTransformer(CompVisionTransformer):
+class DistilledVisionTransformer(VisionTransformer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.dist_token = nn.Parameter(torch.zeros(1, 1, self.embed_dim))
@@ -32,10 +28,27 @@ class DistilledVisionTransformer(CompVisionTransformer):
         trunc_normal_(self.pos_embed, std=.02)
         self.head_dist.apply(self._init_weights)
 
-    def forward(self, sample):
-        x, K, M = sample
-        x = self.comp_forward_afterK(x, ['lastBLK'], K, M)
-        x, x_dist = x[0][:, 0], x[0][:, 1]
+    def forward_features(self, x):
+        # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
+        # with slight modifications to add the dist_token
+        B = x.shape[0]
+        x = self.patch_embed(x)
+
+        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        dist_token = self.dist_token.expand(B, -1, -1)
+        x = torch.cat((cls_tokens, dist_token, x), dim=1)
+
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        x = self.norm(x)
+        return x[:, 0], x[:, 1]
+
+    def forward(self, x):
+        x, x_dist = self.forward_features(x)
         x = self.head(x)
         x_dist = self.head_dist(x_dist)
         if self.training:
@@ -47,7 +60,7 @@ class DistilledVisionTransformer(CompVisionTransformer):
 
 @register_model
 def deit_tiny_patch16_224(pretrained=False, **kwargs):
-    model = CompVisionTransformer(
+    model = VisionTransformer(
         patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     model.default_cfg = _cfg()
@@ -62,7 +75,7 @@ def deit_tiny_patch16_224(pretrained=False, **kwargs):
 
 @register_model
 def deit_small_patch16_224(pretrained=False, **kwargs):
-    model = CompVisionTransformer(
+    model = VisionTransformer(
         patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     model.default_cfg = _cfg()
@@ -77,7 +90,7 @@ def deit_small_patch16_224(pretrained=False, **kwargs):
 
 @register_model
 def deit_base_patch16_224(pretrained=False, **kwargs):
-    model = CompVisionTransformer(
+    model = VisionTransformer(
         patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     model.default_cfg = _cfg()
@@ -137,28 +150,13 @@ def deit_base_distilled_patch16_224(pretrained=False, **kwargs):
 
 @register_model
 def deit_base_patch16_384(pretrained=False, **kwargs):
-    model = CompVisionTransformer(
+    model = VisionTransformer(
         img_size=384, patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     model.default_cfg = _cfg()
     if pretrained:
         checkpoint = torch.hub.load_state_dict_from_url(
             url="https://dl.fbaipublicfiles.com/deit/deit_base_patch16_384-8de9b5d1.pth",
-            map_location="cpu", check_hash=True
-        )
-        model.load_state_dict(checkpoint["model"])
-    return model
-
-
-@register_model
-def deit_base_distilled_patch16_384(pretrained=False, **kwargs):
-    model = DistilledVisionTransformer(
-        img_size=384, patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
-    model.default_cfg = _cfg()
-    if pretrained:
-        checkpoint = torch.hub.load_state_dict_from_url(
-            url="https://dl.fbaipublicfiles.com/deit/deit_base_distilled_patch16_384-d0272ac0.pth",
             map_location="cpu", check_hash=True
         )
         model.load_state_dict(checkpoint["model"])
