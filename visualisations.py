e17a556ad55ecf7fa97da3dc5eac2a6b6fd3053b
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-10-30 01:46:24 +0100

    visualisation refactor

--- visualisations.py
+++ visualisations.py
@@ -190,7 +190,7 @@ def tsne(features, classes_to_render):
     #embd_ncvis = embedder_ncvis.fit_transform(x)
     embd_ncvis = TSNE(n_components=2, learning_rate='auto', init = 'random', perplexity = 3).fit_transform(x)
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(10, 10))
     plt.scatter(*embd_ncvis.T, c=y, alpha=0.5, s=1.0, cmap="tab10", edgecolor="none")
     plt.gca().set_aspect("equal")
     plt.axis("off")
