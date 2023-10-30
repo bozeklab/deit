371a843b9e0e5a570faca47d48fb067a70c7ebab
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-10-30 12:05:48 +0100

    visualize last attn

--- visualize_attention.py
+++ visualize_attention.py
@@ -212,8 +212,14 @@ if __name__ == '__main__':
     attentions = model.last_attn[11]
     nh = attentions.shape[1] # number of head
 
+    print('!!!!')
+    print(attentions.shape)
+
     # we keep only the output patch attention
     attentions = attentions[0, :, 0, 1:].reshape(nh, -1)
+
+    print('!!---!!')
+    print(attentions.shape)
 
     if args.threshold is not None:
         # we keep only a certain percentage of the mass
