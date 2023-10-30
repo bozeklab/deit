86e1abce50190e10526451e309e52a77fc4c8bbf
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-10-30 11:21:07 +0100

    visualize last attn

--- visualize_pca.py
+++ visualize_pca.py
@@ -247,12 +247,12 @@ def main(args):
     model, data_loader = main_setup(args)
 
     ret_dict = extract_patches_k16(data_loader, model, args.device, random_masks=False)
-    for k in range(0, 12):
-        features = np.load(f"/data/pwojcik/deit/debug/extract_k16/{k}_16.npz")
-        tsne(features, k, in1k_classes)
+    #for k in range(0, 12):
+    #    features = np.load(f"/data/pwojcik/deit/debug/extract_k16/{k}_16.npz")
+        #tsne(features, k, in1k_classes)
 
     #PCA_path_tokens_seg(ret_dict)
-    #PCA_path_tokens_rgb(ret_dict)
+    PCA_path_tokens_rgb(ret_dict)
 
 
 if __name__ == '__main__':
