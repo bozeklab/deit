52d5d87fe57090f3af2177329454049a6ff44d1c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-11-02 21:28:54 +0100

    pascal voc

--- pascal_voc.py
+++ pascal_voc.py
@@ -127,5 +127,6 @@ if __name__ == '__main__':
         objs = annotation_info['objects']
         if len(annotation_info['masks']) > 0:
             mask = annotation_info['masks'][0]
-            unique = np.unique(mask).tolist()[1 :-1]
+            unique = np.unique(mask).tolist()[1:-1]
+            print(unique)
         print()
