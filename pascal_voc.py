3e6b5d2f982d593e9a4be226c58817679e0380c5
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-11-02 17:26:43 +0100

    pascal voc

--- pascal_voc.py
+++ pascal_voc.py
@@ -57,7 +57,7 @@ def parse_annotation_and_mask(annotation_path, masks_dir):
         from torchvision import transforms
         mask = transforms.Compose([transforms.ToTensor(mask)])
         int_image = (mask * 255.0).to(torch.uint8)
-        masks.append(mask)
+        masks.append(int_image)
 
     return {
         'image_path': image_path,
