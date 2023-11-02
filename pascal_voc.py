bc5447b892b8c0031a5d05bd6066496d5dd1d1d6
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-11-02 23:13:23 +0100

    pascal voc

--- pascal_voc.py
+++ pascal_voc.py
@@ -174,7 +174,8 @@ if __name__ == '__main__':
         if len(annotation_info['masks']) > 0:
             mask = annotation_info['masks'][0]
             unique = np.unique(mask).tolist()[1:-1]
-            print(unique)
+            if len(unique) == 0:
+                continue
             jac = 0
             for o in unique:
                 masko = mask == o
