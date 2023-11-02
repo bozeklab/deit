b8686d23c3e6f778934625abbfe2eed1f9765657
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-11-02 13:03:57 +0100

    pascal voc

--- pascal_voc.py
+++ pascal_voc.py
@@ -61,30 +61,6 @@ def parse_annotation_and_mask(annotation_path, masks_dir):
         'masks': masks
     }
 
-def main():
-    dataset_dir = '/data/pwojcik/VOCdevkit/VOC2012/'
-    validation_image_set_file = os.path.join(dataset_dir, 'ImageSets/Segmentation/val.txt')
-    annotations_dir = os.path.join(dataset_dir, 'Annotations')
-    masks_dir = os.path.join(dataset_dir, 'SegmentationObject')
-
-    with open(validation_image_set_file, 'r') as f:
-        validation_image_list = f.read().strip().split('\n')
-
-    for image_id in validation_image_list:
-        annotation_file = os.path.join(annotations_dir, image_id + '.xml')
-        annotation_info = parse_annotation_and_mask(annotation_file, masks_dir)
-
-        image_path = os.path.join(dataset_dir, 'JPEGImages', annotation_info['image_path'])
-        print(f'Image Path: {image_path}')
-        print(f'Width: {annotation_info["width"]}, Height: {annotation_info["height"]}')
-
-        for i, obj in enumerate(annotation_info['objects']):
-            print(f'Object: {obj["name"]}')
-            print(f'Bounding Box: {obj["bbox"]}')
-            print()
-            mask = annotation_info['masks'][i]
-            print(f'Mask Shape: {mask.shape}')
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Visualize Self-Attention maps')
@@ -125,3 +101,26 @@ if __name__ == '__main__':
         msg = model.load_state_dict(checkpoint['model'])
         print("Loaded checkpoint: ", msg)
     # open image
+
+    dataset_dir = '/data/pwojcik/VOCdevkit/VOC2012/'
+    validation_image_set_file = os.path.join(dataset_dir, 'ImageSets/Segmentation/val.txt')
+    annotations_dir = os.path.join(dataset_dir, 'Annotations')
+    masks_dir = os.path.join(dataset_dir, 'SegmentationObject')
+
+    with open(validation_image_set_file, 'r') as f:
+        validation_image_list = f.read().strip().split('\n')
+
+    for image_id in validation_image_list:
+        annotation_file = os.path.join(annotations_dir, image_id + '.xml')
+        annotation_info = parse_annotation_and_mask(annotation_file, masks_dir)
+
+        image_path = os.path.join(dataset_dir, 'JPEGImages', annotation_info['image_path'])
+        print(f'Image Path: {image_path}')
+        print(f'Width: {annotation_info["width"]}, Height: {annotation_info["height"]}')
+
+        for i, obj in enumerate(annotation_info['objects']):
+            print(f'Object: {obj["name"]}')
+            print(f'Bounding Box: {obj["bbox"]}')
+            print()
+            mask = annotation_info['masks'][i]
+            print(f'Mask Shape: {mask.shape}')
