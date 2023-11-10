2b06f247a0b5a8ebbdb282d02ec642aa565c55a6
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-11-10 14:19:02 +0100

    pascal voc

--- models_v2.py
+++ models_v2.py
@@ -411,6 +411,7 @@ class vit_models(nn.Module):
 
     @property
     def last_attn(self):
+        print(self.blocks[-1].last_attn)
         return self.blocks[-1].last_attn
 
     def forward(self, x, K=0, masks=None, seq=False, cls_only=False):
