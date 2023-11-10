a2bf3fed99481f4845b9d87cb655a43d42ffd805
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-11-10 15:36:11 +0100

    pascal voc

--- models_dino.py
+++ models_dino.py
@@ -126,8 +126,8 @@ class PatchEmbed(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
         super().__init__()
         num_patches = (img_size // patch_size) * (img_size // patch_size)
-        self.img_size = img_size
-        self.patch_size = patch_size
+        self.img_size = (img_size, img_size)
+        self.patch_size = (patch_size, patch_size)
         self.num_patches = num_patches
 
         self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
