38ab2e03ebbb63986cc8fa39ddbffba1feada24b
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-11-11 18:40:35 +0100

    pascal voc

--- pascal_voc.py
+++ pascal_voc.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
             annotation_info = parse_annotation_and_mask(annotation_file, masks_dir, args.image_size)
 
             image_path = os.path.join(dataset_dir, 'JPEGImages', annotation_info['image_path'])
-            print(f'Image Path: {image_path}')
+            #print(f'Image Path: {image_path}')
             #print(f'Width: {annotation_info["width"]}, Height: {annotation_info["height"]}')
 
             img = transform(Image.open(image_path))
