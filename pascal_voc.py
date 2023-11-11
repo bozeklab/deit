dee8f42a0c12777be716efed858c7543d8b3be54
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-11-11 20:15:26 +0100

    pascal voc

--- pascal_voc.py
+++ pascal_voc.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
     if args.checkpoint is not None:
         #checkpoint = torch.load(args.checkpoint, map_location='cpu')['teacher']
         url = "https://dl.fbaipublicfiles.com/dino/" + "dino_deitsmall8_300ep_pretrain/dino_deitsmall8_300ep_pretrain.pth"
-        checkpoint = torch.hub.load_state_dict_from_url(url=url)
+        pretrained_dict = torch.hub.load_state_dict_from_url(url=url)
         #pretrained_dict = {k.replace('backbone.', ''): v for k, v in checkpoint.items()}
         #pretrained_dict['pos_embed'] = pretrained_dict['pos_embed'][:, 1:, :]
         #utils.interpolate_pos_embed(model, pretrained_dict)
