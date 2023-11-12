1a2d23bffd84f16e03752ed557bb2368dbee99ac
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-11-12 13:49:00 +0100

    video segmentation

--- eval_video_segmentation.py
+++ eval_video_segmentation.py
@@ -309,7 +309,7 @@ if __name__ == '__main__':
         video_name = video_name.strip()
         video_name = video_name[:video_name.rfind('/')]
         print(f'[{i}/{len(video_list)}] Begin to segmentate video {video_name}.')
-        video_dir = os.path.join(args.data_path, "JPEGImages/480p/", video_name)
+        video_dir = os.path.join(args.data_path, video_name)
         print(args.data_path, video_dir, video_name)
         frame_list = read_frame_list(video_dir)
         seg_path = frame_list[0].replace("JPEGImages", "Annotations").replace("jpg", "png")
