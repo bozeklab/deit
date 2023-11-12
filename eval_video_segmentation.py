aac32faf360a26bd843825864adc2bacf041925b
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-11-12 14:44:43 +0100

    video segmentation

--- eval_video_segmentation.py
+++ eval_video_segmentation.py
@@ -306,11 +306,9 @@ if __name__ == '__main__':
 
     video_list = open(os.path.join(args.data_path, "ImageSets/480p/val.txt")).readlines()
     for i, video_name in enumerate(video_list):
-        video_name = video_name.split()[0]
         video_name = video_name.strip()
-        video_name = video_name[:video_name.rfind('/')][1:]
         print(f'[{i}/{len(video_list)}] Begin to segmentate video {video_name}.')
-        video_dir = os.path.join(args.data_path, video_name)
+        video_dir = os.path.join(args.data_path, "JPEGImages/480p/", video_name)
         frame_list = read_frame_list(video_dir)
         seg_path = frame_list[0].replace("JPEGImages", "Annotations").replace("jpg", "png")
         first_seg, seg_ori = read_seg(seg_path, args.patch_size)
