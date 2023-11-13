7757e052b34111273fa3eb7b55514c28cd9b0612
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-11-13 11:08:57 +0100

    video segmentation

--- eval_video_segmentation.py
+++ eval_video_segmentation.py
@@ -162,7 +162,7 @@ def extract_feature(model, frame, device, return_h_w=False):
     elif frame.shape[2] == 896:
         division_masks = DIVISION_SPECS_896[16]
     #division_masks = DIVISION_MASKS[480 // model.patch_embed.patch_size[0]]
-    masks = division_masks[16][0]
+    masks = division_masks[0]
     out = model.get_intermediate_layers_forward_afterK(frame.unsqueeze(0).to(device), K=4, masks=masks, n=1)[0]
     out = out[:, 1:, :]  # we discard the [CLS] token
     h, w = int(frame.shape[1] / model.patch_embed.patch_size[0]), int(frame.shape[2] / model.patch_embed.patch_size[1])
